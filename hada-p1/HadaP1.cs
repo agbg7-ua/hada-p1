7bd503a11d9a0d45d73f6794eede86e8d3001886
agbg7 <agbg7@alu.ua.es>
2023-02-24 11:34:45 +0100

    Comentar HadaP1.cs

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -8,28 +8,30 @@ namespace hada_p1
 {
     class HadaP1
     {
+        //Método que calcula los minutos equivalentes a ciertos segundos dados
         public static double Seconds2Minutes(double s)
         {
-            double i = 0;
-            if(s == 0)
+            double i = 0; //Variable para guardar el resultado de la operación
+            if(s == 0) //Si se dan 0 segundos el resultado de minutos es 0
             {
                 i = 0;
             }
             else
-            {
+            {//Para lo demás, el resultado es dividir los segundos entre 60
                 i = s / 60;
             }
             return i;
         }
+        //Método que calcula los segundos equivalentes a ciertos minutos dados
         public static double Minutes2Seconds(double m)
         {
-            double i = 0;
-            if (s == 0)
+            double i = 0;//Variable para guardar el resultado de la operación
+            if (s == 0)//Si se dan 0 minutos el resultado de segundos es 0
             {
                 i = 0;
             }
             else
-            {
+            {//Para lo demás, el resultado es multiplicar los minutos por 60
                 i = s * 60;
             }
             return i;
